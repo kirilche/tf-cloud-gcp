072b05178ced5ef468d2709fc0b4640402b174dc
Kiril Chervoniak <kiril.chervoniak@gmail.com>
2020-08-11 18:38:17 +0300

    initial vm IP output

---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable project_id {
+    type = string
+    description = "in the terraform.tfvars"
+}
+
+variable region {
+    type = string
+    description = "in the terraform.tfvars"
+}
+
+variable zone {
+    type = string
+    description = "in the terraform.tfvars"
+}
+
+variable credentials {
+    type = string
+    description = "in the terraform.tfvars"
+}
